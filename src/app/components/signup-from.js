90f6ce5609ce3797408c18a015414ec6782841cc
maxmulla23 <maxmulla23@gmail.com>
2024-01-31 14:30:59 +0300

    signup from created

---
+++ src/app/components/signup-from.js
@@ -0,0 +1,45 @@
+'use client'
+import { Button, Card, CardBody, CardFooter, CardHeader, Input, Typography } from "@material-tailwind/react";
+import React from "react";
+
+export default function SignupForm() {
+    return(
+        <Card className="w-96">
+            <CardHeader
+                variant="gradient"
+                color="teal"
+                className="mb-4 grid h-28 place-items-center"
+                >
+                    <Typography variant="h3" color="white">
+                        Create Account
+                    </Typography>
+                </CardHeader>
+                <CardBody className="flex flex-col gap-4">
+                    <Input
+                        color="teal"
+                        type="text"
+                        label="first name" size="lg" />
+                    <Input
+                        color="teal"
+                        type="text"
+                        label="last name" size="lg" />
+                    <Input
+                        type="email"
+                        color="teal" 
+                        label="email address" size="lg" />
+                    <Input
+                        type="password"
+                        color="teal"
+                        label="enter password" size="lg" />
+                </CardBody>
+                <CardFooter className="pt-0">
+                    <Button
+                    variant="filled"
+                    color="teal"
+                    fullWidth>
+                        Create Account
+                    </Button>
+                </CardFooter>
+        </Card>
+    )
+}
