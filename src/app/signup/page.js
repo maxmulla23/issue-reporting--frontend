4deb80cdc5155950a09afd2b5ee5cd96c0303160
maxmulla23 <maxmulla23@gmail.com>
2024-02-27 11:26:42 +0300

    sign up added

---
+++ src/app/signup/page.js
@@ -0,0 +1,13 @@
+'use client'
+
+import SignupForm from "../components/signup-from"
+
+export default function Page(){
+    return (
+        <>
+        <div className="flex justify-center items-center mt-10">
+        <SignupForm />
+        </div>
+        </>
+    )
+}
