702d726a22c1bbce0a9dcab657d70851d4b8f579
maxmulla23 <maxmulla23@gmail.com>
2024-03-07 14:50:59 +0300

    get all devs

--- src/app/dashboard/developers/page.js
+++ src/app/dashboard/developers/page.js
@@ -1,11 +1,22 @@
 'use client'
 
-import { Button, Typography } from "@material-tailwind/react"
+import { Button, Card, Typography } from "@material-tailwind/react"
 
-const TABLE_HEAD = ["Name", "Email", "Phone Number"]
 
-export default function Page()
+const TABLE_HEAD = ["Name", "Email", " "]
+
+async function getData() {
+    const res = await fetch('http://localhost:5030/api/Developer');
+    if(!res.ok)
+    {
+        throw new Error('failed')
+    }
+    return res.json();
+}
+
+export default async function Page()
 {
+    const data = await getData();   
     return(
         <div className="mt-10">
             <Typography variant="h2" color="teal">Developers</Typography>
@@ -15,8 +26,40 @@ export default function Page()
                 <thead>
                     <tr>
                         
+                        {TABLE_HEAD.map((head) => (
+                            <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                                <Typography 
+                                    variant="small"
+                                    color="blue-gray"
+                                    className="font-normal leading-none opacity-70"
+                                >
+                                    {head}
+                                </Typography>
+                            </th>
+                        ))}
                     </tr>
                 </thead>
+                <tbody>
+                    {data.map((developers)=> (
+                        <tr key={developers} className="even:bg-blue-gray-50/50">
+                            <td className="p-4">
+                                <Typography variant="small" color="blue-gray" className="font-normal">
+                                {developers.fullName}
+                                </Typography>   
+                            </td>
+
+                            <td className="p-4">
+                                <Typography variant="small" color="blue-gray" className="font-normal">
+                                {developers.email}
+                                </Typography>   
+                            </td>
+                            <td className="p-4">
+                                <Button color="teal">Edit</Button>
+                            </td>
+                        </tr>
+                        
+                    ))}
+                </tbody>
             </table>
         </Card>
 
