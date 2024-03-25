3ceae4ad54c5b585be5ac08391d23d7f6079f40e
maxmulla23 <maxmulla23@gmail.com>
2024-03-25 11:32:27 +0300

    recommendation table

---
+++ prisma/migrations/20240325083159_recommendation/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Recommendation` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `userId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Recommendation` ADD CONSTRAINT `Recommendation_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
