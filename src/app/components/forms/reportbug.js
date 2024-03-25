bfadf4decb0feb0f2f3da1b4e3b1f9127ad32e1d
maxmulla23 <maxmulla23@gmail.com>
2024-03-25 19:05:37 +0300

    issue form

--- src/app/components/forms/reportbug.js
+++ src/app/components/forms/reportbug.js
@@ -1,8 +1,47 @@
 'use client'
 
-import { Button, Card, CardBody, CardFooter, CardHeader, Input, Typography } from "@material-tailwind/react"
+import { Button, Card, CardBody, CardFooter, CardHeader, Input, Textarea, Typography } from "@material-tailwind/react"
+import  React from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export default function ReportBugform(){
+    const [formData, setFormData] = React.useState({
+        title: "",
+        description: ""
+    })
+    console.log(formData)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            let newFormdata = {
+                ...formData,  
+            }
+            console.log(newFormdata)
+            const response = await axios.post("/api/issue", newFormdata)
+            console.log(response)
+            toast.success("Your bug report has been submitted!")
+        } catch (error) {
+            toast.error("Error occurred!");
+        }
+    }
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const  value = e.target.value;
+
+        const currentInputFieldData = {
+            [name]: value,
+        }
+        const updatedData = {
+            ...formData,
+            ...currentInputFieldData,
+        }
+        setFormData(updatedData)
+    }
+
+
     return (
         <>
         <div>
@@ -17,17 +56,30 @@ export default function ReportBugform(){
                     
                 </CardHeader>
                 <CardBody className="flex flex-col gap-4">
-                    <Input 
+                    <Input
+                    onChange={handleChange}
+                    color="teal"
+                    type="text"
+                    label="title"
+                    size="lg" 
+                    name="title"
+                    
+                    />
+                    <Textarea
+                        onChange={handleChange}
                         color="teal"
                         type="text"
-                        label="Description" size="lg" />
+                        label="Description" 
+                        size="lg"
+                        name="description" 
+                        />
                     
                 </CardBody>
                 <CardFooter>
                     <Button
                     variant="filled"
                     color="teal"
-                    
+                    onClick={handleSubmit}
                     >
                         Submit
                     </Button>
